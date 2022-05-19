93fc42b5002a73eb215b9382b313874b29c0f979
JadeePersad <jadeepersad@yahoo.com>
2022-05-19 19:46:52 -0400

    publish

--- app.py
+++ app.py
@@ -1,101 +1,67 @@
-from flask import Flask,jsonify, request
-import datetime
+from flask import Flask,jsonify, request, json
+from bson.json_util import dumps
+from json import loads
+import os
+from dotenv import load_dotenv
+from flask_pymongo import PyMongo
+from marshmallow import Schema, fields, ValidationError
+
 
 app=Flask(__name__)
-user={}
-time=datetime.datetime.now()
-array1=[]
-id=0
-@app.route('/', methods=['GET'])
 
-@app.route('/profile',methods=["POST"])
-def profile():
+load_dotenv()
+
+# app.config["MONGO_URI"] = "mongodb+srv://ECSE3038_Lab3:ENiP6n3GhCogFtm4@cluster0.rnpwr.mongodb.net/lab3?retryWrites=true&w=majority"
+app.config["MONGO_URI"]=os.getenv("MONGO_CONNECTION_STRING")
+mongo = PyMongo(app)
+
+class TankS(Schema):
+    location = fields.String(required=True)
+    lat = fields.Float(required=True)
+    long = fields.Float(required=True)
+    percentage_full = fields.Integer(required=True)
+
+
+
+@app.route('/data', methods=['GET'])    
+def tankr2():
+    T = mongo.db.lab3tank.find()
+    return jsonify(loads(dumps(T)))
+
     
-    global time
-    time =datetime.datetime.now()
-    global user
-    user = {
-        "last_updated" : time,
-        "username": request.json["username"],
-        "role" : request.json["role"],
-        "color" : request.json["color"]
-    }
-
-    return {"data":user}
-
-@app.route('/profile', methods=['GET'])
-def profile2():
-    return {"data":user}
-
-@app.route('/profile', methods=['PATCH'])
-def profile3():
-    
-    global time 
-    time = datetime.datetime.now()
-
-    if "username" in request.json:
-      user["username"] = request.json["username"]
-
-    if "role" in request.json:
-      user["role"] = request.json["role"]
-
-    if "color" in request.json:
-      user["color"] = request.json["color"]
-      
-    return {"data": user}
-
-
 @app.route('/data',methods=["POST"])
 def data():
-    global tankr
-    global id
-    id= id + 1
-    tankr = {
-        "id":id,
-        "location":request.json["location"],
-        "lat":request.json["lat"],
-        "long":request.json["long"],
-        "percentage_full":request.json["percentage_full"]   
-    }
-    array1.append(tankr)
-    return jsonify(tankr)
-
-
-@app.route('/data', methods=['GET'])
-def tankr2():
-    return jsonify (array1)   
-
-@app.route('/data/<int:id>', methods=['PATCH'])
-def tankr3(id):  
-    for a in array1:
     
-        if id == a["id"]:
-       
-            if "location" in request.json:
-                a["location"] = request.json["location"]    
-            
-            if "lat" in request.json:
-                a["lat"] = request.json["lat"]
-            
-            if "long" in request.json:
-                a["long"] = request.json["long"]
-            
-            if "percentage_full" in request.json:
-                a["percentage_full"] = request.json["percentage_full"]
-
-    return jsonify(array1[int(id)-1])
+    try:
+        tankr=TankS().load(request.json)
+        idtankr = mongo.db.lab3tank.insert_one(tankr).inserted_id
+        tank = mongo.db.lab3tank.find_one(idtankr)
+        return loads(dumps(tank)) 
+    except ValidationError as ve:
+        return ve.messages, 404
+   
 
 
-@app.route('/data/<int:id>', methods=["DELETE"])
+
+
+@app.route('/data/<ObjectId:id>', methods=['PATCH'])
+def tankr3(id):  
+
+    mongo.db.lab3tank.update_one({"_id":id},{"$set":request.json})
+    tank = mongo.db.lab3tank.find_one(id)
+    return loads(dumps(tank))
+
+
+@app.route('/data/<ObjectId:id>', methods=["DELETE"])
 def tankr4(id):
  
-  for a in array1:
-    
-    if id == a["id"]:
-      array1.remove(a)
-  
-  return {"success": True}
-
+    result = mongo.db.lab3tank.delete_one({"_id":id})
+ 
+    if result.deleted_count == 1:
+        return {"success": True}
+   
+    else:
+        return {"success": False }, 404
 
 
 if __name__ == "__main__":
